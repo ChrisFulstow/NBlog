61f77a8ccad264033b7ccfb65cd4fe9acb4ca82b
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2011-12-26 20:52:18 -0500

    Implementing automatic build number generation

---
+++ NBlog.Web/Application/Infrastructure/BuildNumberProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace NBlog.Web.Application.Infrastructure
+{
+    /// <summary>
+    /// Provides current build number
+    /// </summary>
+    public static class BuildNumberProvider
+    {
+        /// <summary>
+        /// Returs
+        /// </summary>
+        /// <returns>A string containing Major, major rev, minor, minor rev and Revision</returns>
+        public static string GetBuildNumber()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
