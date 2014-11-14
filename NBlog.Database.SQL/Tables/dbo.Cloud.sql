424ac72b90121f7da7693f5563c0e664a8295c48
Matt Carr <mattc@magenic.com>
2014-11-14 14:22:41 -0600

    Adding sql project and table scripts for creating tables

---
+++ NBlog.Database.SQL/Tables/dbo.Cloud.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Cloud]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	[ConsumerKey] NVARCHAR(250) NOT NULL DEFAULT 'DropBox_Key',
+	[ConsumerSecret] NVARCHAR(250) NOT NULL DEFAULT 'DropBox_Secret',
+	[UserToken] NVARCHAR(250) NOT NULL DEFAULT 'DropBox_UserToken',
+	[UserSecret] NVARCHAR(250) NOT NULL DEFAULT 'DropBox_UserSecret'
+)
---
+++ NBlog.Database.SQL/Tables/dbo.ContactForm.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ContactForm]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	[RecipientName] NVARCHAR(250) NOT NULL DEFAULT 'NBlog Admin',
+	[RecipientEmail] NVARCHAR(250) NOT NULL DEFAULT 'nblog-admin@mailinator.com',
+	[Subject] NVARCHAR(250) NOT NULL DEFAULT 'NBlog contact form'
+)
